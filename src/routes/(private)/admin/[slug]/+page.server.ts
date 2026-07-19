5952df2767bbdf1a891596b83c090c8de0069daf
pwhb <phonewaiyamhein@gmail.com>
2026-07-19 17:52:57 +0630

    fix: make fetch api conf dynamic

--- src/routes/(private)/admin/[slug]/+page.server.ts
+++ src/routes/(private)/admin/[slug]/+page.server.ts
@@ -1,7 +1,12 @@
+import { fillTemplate } from '$lib/client/common';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ fetch, url, params }) => {
-	const res = await fetch(`/api/${params.slug}${url.search}`);
+export const load: PageServerLoad = async ({ fetch, url, params, locals }) => {
+	const apiConfig = locals.pageConfig.fetch;
+	const res = await fetch(`${fillTemplate(apiConfig.url, params)}${url.search}`, {
+		method: apiConfig.method,
+		headers: apiConfig.headers
+	});
 	if (res.ok) {
 		const pageData = await res.json();
 		return { pageData };
--- src/routes/(private)/admin/[slug]/edit/[id]/+page.server.ts
+++ src/routes/(private)/admin/[slug]/edit/[id]/+page.server.ts
@@ -1,7 +1,12 @@
+import { fillTemplate } from '$lib/client/common';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ params, fetch }) => {
-	const res = await fetch(`/api/${params.slug}/${params.id}`);
+export const load: PageServerLoad = async ({ params, fetch, locals }) => {
+	const apiConfig = locals.pageConfig.fetch;
+	const res = await fetch(`${fillTemplate(apiConfig.url, params)}`, {
+		method: apiConfig.method,
+		headers: apiConfig.headers
+	});
 	if (res.ok) {
 		const pageData = await res.json();
 		return { pageData };
